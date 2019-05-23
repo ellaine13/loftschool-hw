359ccba20d5ef42d457085cea1eb894b4c2e8397
mikalai_harbunou <n.gorbunov@21vek.by>
2019-05-24 01:18:29 +0300

    Tasks 1, 2, 3, 5.

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {
+function returnFirstArgument(someParameter) {
+    return someParameter;
 }
 
 /*
@@ -29,7 +30,8 @@ function returnFirstArgument() {
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults(a, b) {
+function sumWithDefaults(a, b = 100) {
+    return a + b;
 }
 
 /*
@@ -41,7 +43,14 @@ function sumWithDefaults(a, b) {
    returnFnResult(() => 'привет') вернет 'привет'
  */
 function returnFnResult(fn) {
+    var result = fn();
+
+    return result;
 }
+
+returnFnResult(function(someArgument) {
+    return someArgument;
+});
 
 /*
  Задание 4:
@@ -69,6 +78,13 @@ function returnCounter(number) {
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
 function returnArgumentsArray() {
+    var newArr = [];
+
+    for (var i = 0; i < arguments.length; i++) {
+        newArr.push(arguments[i]);
+    }
+
+    return newArr;
 }
 
 /*
